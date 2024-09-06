a8cdfaa23ada55ed895d236843497a32d2f3446d
shintoyohannan <41267638+shintoyohannan@users.noreply.github.com>
2024-09-06 14:26:09 +0200

    Update steamlit_app.py
    rearranging requests

--- steamlit_app.py
+++ steamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-
+import requests
 # Write directly to the app
 st.title("Customize Your Smoothie :cup_with_straw:")
 st.write(
@@ -30,7 +30,8 @@ if ingredients_list:
 
    for fruit_chosen in ingredients_list:
        ingredients_string += fruit_chosen + ' '
-
+       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+       fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
    #st.write(ingredients_string)
 
    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -45,7 +46,7 @@ if ingredients_list:
        
        st.success('Your Smoothie is ordered!', icon="✅")
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#import requests
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+#fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
