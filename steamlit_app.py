f9d184b1513e07dfd5d18fa75678cee1f22786ac
shintoyohannan <41267638+shintoyohannan@users.noreply.github.com>
2024-09-06 15:19:50 +0200

    Update steamlit_app.py
    import pandas

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,7 +1,8 @@
 # Import python packages
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import col, pandas
+from snowflake.snowpark.functions import col
+import pandas
 import requests
 # Write directly to the app
 st.title("Customize Your Smoothie :cup_with_straw:")
